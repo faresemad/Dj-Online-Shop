b3dd4bc176070da8229745c92ddc5ccf025ef6cd
Fares Emad <faresemadx@gmail.com>
2023-07-12 16:14:15 +0300

    update settings

--- myshop/settings.py
+++ myshop/settings.py
@@ -92,7 +92,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "faresemad47@gmail.com"
-EMAIL_HOST_PASSWORD = "vcpumwfhierxgzrk"
+EMAIL_HOST_PASSWORD = "<your password>"
 EMAIL_USE_TLS = True
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Password validation
